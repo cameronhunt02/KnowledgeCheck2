9b18ea9cd1b6714d151f4369cce79f25c50a1852
cameronhunt02 <138541580+cameronhunt02@users.noreply.github.com>
2023-12-20 20:57:28 -0500

    Add files via upload
    Uploaded knowledge check 2

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+
+Console.WriteLine("How many Monsters do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+var recordList = new List<Monster>();
+
+for (int i = 0; i < numberOfRecords; i++)
+{
+    // In this loop, populate the object's properties using Console.ReadLine()
+    var monster = new Monster();
+
+    Console.WriteLine("Enter the type of monster ");
+    monster.Name = Console.ReadLine();
+
+
+    recordList.Add(monster);
+}
+foreach (var record in recordList)
+{
+    Console.WriteLine($"New monster name: {record.Name}");
+}
+   
+    
+    
+
+
+
+// Print out the list of records using Console.WriteLine()
+
+class Monster
+{
+    public int Hp { get; set; }
+    public string? Name { get; set; }
+}
+class Skeleton : Monster
+{
+    public Skeleton()
+    {
+        Name = "Skeleton";
+    }
+}
+
+
+
